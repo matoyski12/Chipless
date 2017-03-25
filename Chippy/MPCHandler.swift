8cf0dee429c8057104570884b1064ef92d3805b8
Mike Metoyer <mmetoyer1297@gmail.com>
2017-03-25 16:18:40 -0500

    Put in all the lines of code that I botched. Got an error message trying to set up the PeerID

--- Chippy/MPCHandler.swift
+++ Chippy/MPCHandler.swift
@@ -48,3 +48,21 @@ class MPCHandler: NSObject, MCSessionDelegate {
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
     }
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        let = userInfo ["peerID":peerID,"state":state]
+        
+}
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+        
+}
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        <#code#>
+}
+
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+
+    
+    
+}
